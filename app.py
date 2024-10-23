10ecfaaddfd479330586231fd0ffe5c604f9381b
czett <keinaniemand@gmail.com>
2024-10-23 21:39:31 +0200

    error handling and favicon

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, send_file
 import os
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
@@ -14,22 +15,27 @@ def start():
     files = list_files_in_folder(app.config['UPLOAD_FOLDER'])
     return render_template("index.html", files=files)
 
+@app.route("/message/<m>")
+def index_message(m):
+    files = list_files_in_folder(app.config['UPLOAD_FOLDER'])
+    return render_template("index.html", files=files, message=m)
+
 @app.route("/upload", methods=["POST"])
 def upload_file():
     if 'file' not in request.files:
-        return "Keine Datei hochgeladen!"
+        return redirect("/message/file_not_uploaded")
     
     file = request.files['file']
     
     if file.filename == '':
-        return "Keine Datei ausgewählt!"
+        return redirect("/message/no_file_selected")
     
     if file:
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
         return redirect("/")
     
-    return "Fehler beim Hochladen der Datei!"
+    return redirect("/message/internal_error")
 
 @app.route("/delete/<file>")
 def delete(file):
@@ -40,12 +46,16 @@ def delete(file):
 def download(file):
     return send_file(f"files/{file}", as_attachment=True)
 
+@app.errorhandler(Exception)
+def handle_error(e):
+    return redirect("/message/some_error")
+
 def list_files_in_folder(folder):
     try:
         files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
         return files
     except FileNotFoundError:
-        return f"Der Ordner '{folder}' wurde nicht gefunden!"
+        return redirect("/message/dir_not_found")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5400)
